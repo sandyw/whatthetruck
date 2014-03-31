be6a5460e58b5fac4d10c19670f606efed65f4bb
Sandy Weber <sandy.j.weber@gmail.com>
2014-03-31 19:20:41 -0400

    Rails new whatthetruck

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whatthetruck::Application.config.secret_key_base = 'b1453a2def961aaddafce1bf5499940874ff3f7244932f82f077c8b8be3d200a4b6264b3042f1953d81825d57065790c24d09d158290f648b5155a76aa180af8'
